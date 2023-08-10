17a718032a18130932b8302a9058c4978ad2d4f7
g.martino <gianmart83@gmail.com>
2023-08-10 21:41:47 +0200

    modifica per supportare chrome

--- prova.py
+++ prova.py
@@ -1,14 +1,15 @@
-from selenium.webdriver import Firefox
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 opts = Options()
-opts.add_argument('--start-mazimized')
-opts.add_argument('--enable-automation')
-opts.add_argument('--incognito')
-browser = Firefox(options=opts)
+opts.add_argument('start-maximized')
+opts.add_argument('enable-automation')
+opts.add_argument('incognito')
+opts.add_argument('disable-infobars')
+browser = Chrome(options=opts)
 browser.get('https://duckduckgo.com')
 search_form = browser.find_element(By.ID, 'search_form_input_homepage')
 search_form.send_keys('real python')
@@ -17,6 +18,6 @@ try:
     WebDriverWait(browser, 10).until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
 except:
     browser.quit()
-results = browser.find_element(By.ID, 'react-results--main')
-print(results[0].text)
+results = browser.find_element(By.CLASS_NAME, 'react-results--main')
+#print(results[0])
 browser.quit()
